8dc9cad2c1c81623e5bf06340f17e53c1ab65888
Matt Greensmith <matt.greensmith@gmail.com>
2020-03-06 00:07:06 -0800

    Set hostname to balena device name.

--- cups/run.sh
+++ cups/run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Set the hostname to the balena device name
+balena_device_name=$(curl -s "$BALENA_SUPERVISOR_ADDRESS/v2/device/name?apikey=$BALENA_SUPERVISOR_API_KEY" | jq -r .deviceName)
+curl -X PATCH --header "Content-Type:application/json" \
+	--data "{\"network\": {\"hostname\": \"${balena_device_name}\"}}" \
+	"$BALENA_SUPERVISOR_ADDRESS/v1/device/host-config?apikey=$BALENA_SUPERVISOR_API_KEY"
+
 # Tell the container that DBUS should report to Host OS
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
